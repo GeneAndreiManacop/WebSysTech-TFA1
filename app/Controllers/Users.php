3e8ac8ff7227ab9e199d3162da465994d2f014d7
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-09-10 19:18:06 +0800

    Create Users.php

---
+++ app/Controllers/Users.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+class Users extends BaseController
+{
+    public function index(): string
+    {
+        $users = [
+            [
+                'id' => 1, 
+                'username' => 'juan_santos',
+                'fullname' => 'Juan Santos',
+                'role' => 'Administrator'
+            ],
+            [
+                'id' => 2, 
+                'username' => 'maria_garcia',
+                'fullname' => 'Maria Garcia',
+                'role' => 'Manager'
+            ],
+            [
+                'id' => 3, 
+                'username' => 'rafael_cruz',
+                'fullname' => 'Rafael Cruz',
+                'role' => 'Cashier'
+            ],
+            [
+                'id' => 4, 
+                'username' => 'angela_lopez',
+                'fullname' => 'Angela Lopez',
+                'role' => 'Cashier'
+            ],
+            [
+                'id' => 5, 
+                'username' => 'daniel_torres',
+                'fullname' => 'Daniel Torres',
+                'role' => 'Staff'
+            ],
+        ];
+        return view('users', ['users' => $users]);
+    }
+  
+}
