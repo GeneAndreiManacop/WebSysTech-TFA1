21e200243a5278182178ace08ff353644457b5dc
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-09-10 21:47:22 +0800

    Create navbar.php

---
+++ app/Views/components/navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <h1 class="brand">P.O.S System</h1>
+    <ul class="navigation">
+        <li><a href="<?= base_url() ?>">Home</a></li>
+        <li><a href="<?= base_url('about') ?>">About</a></li>
+        <li><a href="<?= base_url('users') ?>">Users</a></li>
+        <li><a href="<?= base_url('customers') ?>">Customers</a></li>
+    </ul>
+</nav>
