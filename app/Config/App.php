e2454da1f0e89f81a056f7a7dc68a1f87b605899
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-09-10 18:58:44 +0800

    Change base URL

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/demonstration_ci4';
+    public string $baseURL = 'http://localhost/WebSysTech-TFA1';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
