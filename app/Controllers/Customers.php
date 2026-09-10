3be465a6af8a8f0a3a4babad439ceaf0acfbf293
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-09-10 19:17:39 +0800

    Create Customers.php

---
+++ app/Controllers/Customers.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controllers;
+
+class Customers extends BaseController
+{
+    
+    public function index(): string
+    {
+        $customers = [
+            [
+                'id' => 1, 
+                'fullname' => 'Andrea Santos',
+                'email' => 'andrea.santos@example.com',
+                'phone' => '09171234567'
+            ],
+            [
+                'id' => 2, 
+                'fullname' => 'Miguel Reyes',
+                'email' => 'miguel.reyes@example.com',
+                'phone' => '09281234567'
+            ],
+            [
+                'id' => 3, 
+                'fullname' => 'Patricia Lim',
+                'email' => 'patricia.lim@example.com',
+                'phone' => '09391234567'
+            ],
+            [
+                'id' => 4, 
+                'fullname' => 'Carlo Mendoza',
+                'email' => 'carlo.mendoza@example.com',
+                'phone' => '09451234567'
+            ],
+            [
+                'id' => 5, 
+                'fullname' => 'Sofia Navarro',
+                'email' => 'sofia.navarro@example.com',
+                'phone' => '09561234567'
+            ],
+        ];
+        return view('customers', ['customers' => $customers]);
+    }
+  
+}
