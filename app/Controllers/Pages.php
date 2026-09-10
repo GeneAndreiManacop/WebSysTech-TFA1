3da1de2be81c8d6c6c16479f2fabf7f7240d5911
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-09-10 19:18:02 +0800

    Create Pages.php

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function home(): string
+    {
+        return view('home');
+    }
+
+    public function about(): string
+    {
+        return view('about');
+    }
+  
+}
